d57bfc50d47988a15e4d0464181834937489b473
joaov <joaovtolente@gmail.com>
2025-10-13 23:31:15 -0300

    Atualizada a estrutura do código, removido workarounds e erros de síntaxe.

--- usage_benchmark/info_display.py
+++ usage_benchmark/info_display.py
@@ -2,73 +2,85 @@ from pc_info import PcInfo
 import time
 import os
 
+
+def display_bar(percent, length=30):
+    bar_progress = int(length * percent / 100)
+    return "█" * bar_progress + "-" * (length - bar_progress)
+
+def bytes_to_num(n_byte):
+    symbols = ("B", "KB", "GB", "TB")
+    step = 1024
+    i = 0
+    while n_byte >= step and i < len(symbols):
+        n_byte /= step
+        i += 1
+    return f"{n_byte:.2f} {symbols[i]}"
+
 class InfoDisplay:
     def __init__(self):
         self.info = PcInfo()
 
-    def display_bar(self, percent, length=30):
+    def display_bar(percent, length=30):
         bar_progress = int(length * percent / 100)
         return "█" * bar_progress + "-" * (length - bar_progress)
+
+    def bytes_to_num(self, n_byte):
+        symbols = ("B", "KB", "GB", "TB")
+        step = 1024.0
+        i = 0
+        while n_byte >= step and i < len(symbols):
+            n_byte /= step
+            i += 1
+        return f"{n_byte:.2f} {symbols[i]}"
+
+    def cpu_display(self, cpu_info):
+        print("// === CPU ===//")
+        current_cpu_frequency = cpu_info['cpu_frequency']['current']
+        max_cpu_frequency = cpu_info['cpu_frequency']['max']
+        percent_cpu_frequency = (current_cpu_frequency / max_cpu_frequency) * 100 if max_cpu_frequency > 0 else 0
+
+        print(f"\nUtilização Total da CPU: "
+              f"\n {cpu_info['cpu_total_usage']:.2f} %"
+              f" | {display_bar(cpu_info['cpu_total_usage'])} |\n")
+
+        print(f"\nNúcleos: {cpu_info['cpu_physical_cores']}"
+              f"\nProcessadores Lógicos: {cpu_info['cpu_cores']}"
+              f"\nFrequência do Procesador: {cpu_info['cpu_frequency']['current']:.2f} Ghz | "
+              f"{percent_cpu_frequency:5.1f} % {display_bar(percent_cpu_frequency)} |"
+              f"\n"
+              )
+
+        for i, core in enumerate(cpu_info.cpu_info['cpu_core_usage']):
+            print(f"...Core {i + 1:02}: {core:05} % | {display_bar(core)}  |")
+
+    def memory_display(self, memory_info):
+        print("\n// === Memória Ram ===// ")
+
+        print(f"\nUtilização Total da Memória Ram: \n{memory_info['ram_percent']:.2f} %"
+              f" | {display_bar(memory_info['ram_percent'])} |\n"
+              f"\nMemória Ram: {self.bytes_to_num(memory_info['ram_total'])}"
+              f"\nMemória Usada: {self.bytes_to_num(memory_info['ram_used'])}"
+              f"\nMemória Livre: {self.bytes_to_num(memory_info['ram_free'])}"
+              )
+
+    def disk_display(self, disk_info):
+        print(f"\n// === Disco Rígido ===//")
+
+        print(f"\nUtilização Total do Disco: \n{disk_info['disk_percent']:.2f} %"
+              f" | {display_bar(disk_info['disk_percent'])} |\n"
+              f"\nEspaço Total: {self.bytes_to_num(disk_info['disk_total'])}"
+              f"\nEspaço Usado: {self.bytes_to_num(disk_info['disk_used'])}"
+              f"\nEspaço Livre: {self.bytes_to_num(disk_info['disk_free'])}")
 
     def display(self, interval=1):
         try:
             while True:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 self.info.collector()
-                display_values = self.info
-
-                ## 1. Isso ainda será devidamente separado em uma classes e defs dedicadas*, com cada valor convertido em string
-                ## CPU
-
-                print("// === CPU ===// ")
-
-                current_cpu_frequency = display_values.cpu_info['cpu_frequency']['current']
-                max_cpu_frequency = display_values.cpu_info['cpu_frequency']['max']
-                percent_cpu_frequency = (current_cpu_frequency / max_cpu_frequency) * 100 if max_cpu_frequency > 0 else 0
-
-                print(f"\nUtilização Total da CPU: "
-                      f"\n {display_values.cpu_info['cpu_total_usage']:.2f} %"
-                      f" | {self.display_bar(display_values.cpu_info['cpu_total_usage'])} |\n")
-
-                print(f"\nNúcleos: {display_values.cpu_info['cpu_physical_cores']}"
-                      f"\nProcessadores Lógicos: {display_values.cpu_info['cpu_cores']}"
-                      f"\nFrequência do Procesador: {display_values.cpu_info['cpu_frequency']['current']:.2f} Ghz | "
-                      f"{percent_cpu_frequency:5.1f} % {self.display_bar(percent_cpu_frequency)} |"
-                      f"\n"
-                      )
-
-                for i, core in enumerate(display_values.cpu_info['cpu_core_usage']):
-                    print(f"...Core {i+1:02}: {core:05} % | {self.display_bar(core)}  |")
-
-
-                ## Memória
-                ## Converter corretamente o tamanho da memória*, atribuindo apenas ao tamanho útil
-
-                memory_size_div = 1000000 ## Diminuir por 6 '0's.
-
-                print("\n// === Memória Ram ===// ")
-
-                print(f"\nUtilização Total da Memória Ram: \n{display_values.memory_info['ram_percent']:.2f} %"
-                        f" | {self.display_bar(display_values.memory_info['ram_percent'])} |\n"
-                        f"\nMemória Ram: {display_values.memory_info['ram_total']/memory_size_div:.2f} MB"
-                        f"\nMemória Usada: {display_values.memory_info['ram_used']/memory_size_div:.2f} MB"
-                        f"\nMemória Livre: {display_values.memory_info['ram_free']/memory_size_div:.2f} MB"
-                        )
-
-                ## Disco Rígido
-
-                disk_size_div = 1000000000 # Diminuir por 9 '0's.
-
-                print(f"\n// === Disco Rígido ===//")
-
-                print(f"\nUtilização Total do Disco: \n{display_values.disk_info['disk_percent']:.2f} %"
-                      f" | {self.display_bar(display_values.disk_info['disk_percent'])} |\n"
-                      f"\nEspaço Total: {display_values.disk_info['disk_total'] / disk_size_div:.2f} GB"
-                      f"\nEspaço Usado: {display_values.disk_info['disk_used'] / disk_size_div:.2f} GB"
-                      f"\nEspaço Livre: {display_values.disk_info['disk_free'] / disk_size_div:.2f} GB")
-
+                self.cpu_display(self.info.cpu_info)
+                self.memory_display(self.info.memory_info)
+                self.disk_display(self.info.disk_info)
                 time.sleep(interval)
-
         except KeyboardInterrupt:
             print('\nExit')
 
--- usage_benchmark/pc_info.py
+++ usage_benchmark/pc_info.py
@@ -1,9 +1,6 @@
 import psutil
 
 class PcInfo:
-    def __new__(cls, *args, **kwargs):
-        instance = object.__new__(cls)
-        return instance
 
     def __init__(self):
         self.cpu_info = None
@@ -11,7 +8,7 @@ class PcInfo:
         self.disk_info = None
         self.internet_info = None
 
-    def collector(self):
+    def collect_cpu_info(self):
 
         self.cpu_info = {
             "cpu_physical_cores": psutil.cpu_count(logical=False),
@@ -20,24 +17,36 @@ class PcInfo:
             "cpu_core_usage": psutil.cpu_percent(percpu=True),
             "cpu_total_usage": psutil.cpu_percent(),
         }
+        return self.cpu_info
 
+    def collect_memory_info(self):
         system_virtual_mem = psutil.virtual_memory()
-
         self.memory_info = {
             "ram_total": system_virtual_mem.total,
             "ram_used": system_virtual_mem.used,
             "ram_free": system_virtual_mem.free,
             "ram_percent": system_virtual_mem.percent,
         }
+        return self.memory_info
 
+    def collect_disk_info(self):
         self.disk_info = {
             "disk_total": psutil.disk_usage("/").total,
             "disk_used": psutil.disk_usage("/").used,
             "disk_free": psutil.disk_usage("/").free,
             "disk_percent": psutil.disk_usage("/").percent,
         }
+        return self.disk_info
 
+    def collect_internet_info(self):
         self.internet_info = {
             "internet_bytes_sent": psutil.net_io_counters().bytes_sent,
             "internet_bytes_received": psutil.net_io_counters().bytes_recv,
         }
+        return self.internet_info
+
+    def collector(self):
+        self.collect_cpu_info()
+        self.collect_memory_info()
+        self.collect_disk_info()
+        self.collect_internet_info()
